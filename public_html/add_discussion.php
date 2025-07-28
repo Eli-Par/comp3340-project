74778645f66defeec2a11e7ae31027a1f395d6f5
Eli-Par <epar4620@gmail.com>
2025-07-28 11:59:12 -0400

    Fix discussion fields not being required

--- public_html/add_discussion.php
+++ public_html/add_discussion.php
@@ -25,11 +25,11 @@ $userId = $_SESSION['userId'] ?? 0;
         <form method="post" action="../private/add_discussion_api.php">
             <div>
                 <label for="title">Title</label>
-                <input name="title" id="title" minlength="4" maxlength="200"></input>
+                <input name="title" id="title" required minlength="4" maxlength="200"></input>
             </div>
             <div>
                 <label for="content">Content</label>
-                <textarea name="content" id="content" rows="16"></textarea>
+                <textarea name="content" id="content" required rows="16"></textarea>
             </div>
 
             <button type="submit" style="max-width: 500px; margin: 0 auto;">Add Discussion</button>
